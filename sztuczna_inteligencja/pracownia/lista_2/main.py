8f56f913ca80c4995a2a76dca441a66c53a7a541
Piotr Pijanowski <133985473+Owczany@users.noreply.github.com>
2026-03-22 11:27:03 +0100

    Summary

--- sztuczna_inteligencja/pracownia/lista_2/main.py
+++
@@ -1,2 +0,0 @@
-with open ('zad_output.txt', 'w') as f:
-        f.write('RRUUU')
--- sztuczna_inteligencja/pracownia/lista_2/zad1.py
+++ sztuczna_inteligencja/pracownia/lista_2/zad1.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import random
 from functools import lru_cache
 from typing import List, Tuple
